0c0a721923219935b54a0d1c4c47495076e75ca6
SabineU <hozine3@gmail.com>
2026-04-19 06:43:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h1>Add Preview</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
